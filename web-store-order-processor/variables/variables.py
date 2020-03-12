fd67482a77eff4d8a0796bf53091e67ec1c71f72
Jani Palsamäki <jani.palsamaki@gmail.com>
2020-03-12 13:41:01 +0200

    Update env.json, move .xlsx to devdata directory

--- web-store-order-processor/variables/variables.py
+++ web-store-order-processor/variables/variables.py
@@ -1,7 +1,7 @@
 import os
 
 BROWSER = "Chrome"
-EXCEL_FILE_PATH = "./data/Data.xlsx"
+EXCEL_FILE_PATH = "./devdata/Data.xlsx"
 SWAG_LABS_URL = "https://www.saucedemo.com"
 SWAG_LABS_PASSWORD = os.getenv("SWAG_LABS_PASSWORD")
 SWAG_LABS_USER_NAME = os.getenv("SWAG_LABS_USER_NAME")
