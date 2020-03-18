8a7a3e54421a9202807c4521e19c2c98cc7f1546
Jani Palsamäki <jani.palsamaki@gmail.com>
2020-03-18 15:52:31 +0200

    Update entrypoint.sh

--- robocorp-early-access/entrypoints/entrypoint.sh
+++ robocorp-early-access/entrypoints/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 # Executes all tasks defined in the "tasks" directory.
-# Writes log and report files to "output" directory.
+# Writes output to "output" directory.
 robot -d output --logtitle "Task log" tasks/
