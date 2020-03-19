d1b3a5963d7df51269e72b23e8ef5683077d9de6
Jani Palsamäki <jani.palsamaki@gmail.com>
2020-03-19 10:03:41 +0200

    Remove unnecessary webdrivermanager setup

--- simple-web-scraper/entrypoints/entrypoint.sh
+++ simple-web-scraper/entrypoints/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -ex
 
-# Sets up chromedriver for Chrome.
-webdrivermanager chrome -d temp -l bin
-
 # Executes all tasks defined in the "tasks" directory.
 # Writes log and report files to "output" directory.
 robot -d output --logtitle "Task log" tasks/
