f19dcf90cdc6838c54295e19ea5979a90a6365a1
Jani Palsamäki <jani@robocorp.com>
2020-09-17 13:12:59 +0300

    Simplify robotsparebin-complete

--- robotsparebin-starter/entrypoints/entrypoint.cmd
+++ robotsparebin-starter/entrypoints/entrypoint.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-REM Executes all tasks defined in the "tasks" directory.
 REM Writes log and report files to "output" directory.
 python -m robot --report NONE --outputdir output --logtitle "Task log" robot.robot || goto :error
 
